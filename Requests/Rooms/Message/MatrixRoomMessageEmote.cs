ff82dfe1c61781378a32668e86e3e55549ece78b
Craig Richards <craigr3@hotmail.com>
2019-03-10 17:33:33 +0000

    [WIP] Added classes to send emotes and images as a message

---
+++ Requests/Rooms/Message/MatrixRoomMessageEmote.cs
@@ -0,0 +1,14 @@
+﻿using System.Runtime.Serialization;
+
+namespace libMatrix.Requests.Rooms.Message
+{
+    [DataContract]
+    public class MatrixRoomMessageEmote : MatrixRoomMessageBase
+    {
+        public MatrixRoomMessageEmote()
+            : base()
+        {
+            base.MessageType = "m.emote";
+        }
+    }
+}
---
+++ Requests/Rooms/Message/MatrixRoomMessageImage.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace libMatrix.Requests.Rooms.Message
+{
+    [DataContract]
+    public class MatrixRoomMessageImage : MatrixRoomMessageBase
+    {
+        public MatrixRoomMessageImage()
+            : base()
+        {
+            base.MessageType = "m.image";
+        }
+
+        [DataMember(Name = "body", IsRequired = true)]
+        public string Description { get; set; }
+
+        [DataMember(Name = "url", IsRequired = false)]
+        public string ImageUrl { get; set; }
+    }
+}
